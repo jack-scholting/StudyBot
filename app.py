8bc26b67fea2976fea7abce51e4bc0a979115a66
Jack Scholting <jack.scholting@gmail.com>
2017-10-15 15:38:09 -0500

    Minor change to test Heroku auto-deploy.

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def messaging_events(payload):
 def send_message(token, recipient, text):
   """Send the message text to recipient with id recipient.
   """
-  msg_text = "STUDYBOT: " + text.decode('unicode_escape')
+  msg_text = "STUDYBOT ECHO: " + text.decode('unicode_escape')
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
     data=json.dumps({
