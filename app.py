24aac7b4d713c9f1f933150fd20f17f1f5a9e5ea
rinichulous <rinichulous@users.noreply.github.com>
2017-11-25 12:03:14 -0500

    Refactor

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import json
 import requests
 import os
 
 # Create the Flask application instance.
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-import User
+#===============================================================================
+# DB Classes
+#===============================================================================
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    fb_id = db.Column(db.String, unique=True)
+    facts = db.relationship('Fact', lazy='select', backref=db.backref('users', lazy='joined'))
+
+    def __init__(self, fb_id):
+        self.fb_id = fb_id
+
+    def __repr__(self):
+        return '<FB ID %r>' % self.fb_id
+
+
+class Fact(db.Model):
+    __tablename__ = 'facts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    fact = db.Column(db.String, unique=True)
+    last_seen = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    __table_args__ = (
+        db.UniqueConstraint('user_id', 'fact', name='user_id_fact'),
+    )
+
+    def __repr__(self):
+        return '<FB ID - Fact: %r - %r>' % self.fb_id, self.fact
 
 #===============================================================================
 # Global Data
