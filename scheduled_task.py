d5b637f3b5ad84edda4149e0cb03f843e5fa232b
Jack Scholting <jack.scholting@gmail.com>
2017-12-09 16:18:28 -0600

    Fixed bugs.

--- scheduled_task.py
+++ scheduled_task.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
 
     #TODO may need some logic to randomize study prompts.
 
-    all_users = get_all_users()
+    all_users = app.get_all_users()
     for user in all_users:
         if (user.silence_end_time < datetime.datetime.now()):
-            fact = get_next_fact_to_study(user.id)
+            fact = app.get_next_fact_to_study(user.id)
             if (fact):
                 app.send_message(user.fb_id, "Time to study!", False)
                 app.send_message(user.fb_id, fact.question, False)
